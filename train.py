7f49fc3cf48467ff50779a95328ae267e85c8cf6
Taylor Bontrager <ttbontra@gmail.com>
2024-02-23 10:17:31 -0500

    added training augmentation

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 
 # Use the model
-model.train(data="custom_data.yaml", epochs=150, imgsz=640, batch_size=16, workers=8)  
+model.train(data="custom_data.yaml", epochs=150, imgsz=640, workers=8)  
